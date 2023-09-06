6262a5cd1f0aa9f06253c83d14215e2a38aea434
Gregdams <greg.loupy@gmail.com>
2023-09-06 16:14:07 +0900

    add: channel_id from env variables

--- src/constants/common.rs
+++ src/constants/common.rs
@@ -1,10 +1,16 @@
 use serenity::model::prelude::ChannelId;
 
-pub const TWO_FACTOR_DEPLOYMENT_CHANNEL: ChannelId = ChannelId(745312984481398806);
+// pub const TWO_FACTOR_DEPLOYMENT_CHANNEL: ChannelId = ChannelId(745312984481398806);
 
 lazy_static! {
   pub static ref CODEFLOW_SUPERVISOR_URL: String = std::env::var("CODEFLOW_SUPERVISOR_URL")
     .expect("missing CODEFLOW_SUPERVISOR_URL env variables");
+  pub static ref TWO_FACTOR_DEPLOYMENT_CHANNEL: ChannelId = ChannelId(
+    std::env::var("TWO_FACTOR_DEPLOYMENT_CHANNEL")
+      .expect("missing TWO_FACTOR_DEPLOYMENT_CHANNEL env variables")
+      .parse::<u64>()
+      .expect("Failed to parse TWO_FACTOR_DEPLOYMENT_CHANNEL into u64")
+  );
 }
 
 impl CODEFLOW_SUPERVISOR_URL {
